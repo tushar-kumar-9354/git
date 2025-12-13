130f53d09ce0f75feb1261c875f69208820b3e56
tushar-kumar-9354 <jangratushar@348gmail.com>
2025-12-13 11:52:41 +0530

    ..

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path("music/", views.background_music, name="background_music"),
-    path("", views.home, name="home"),
+    path("", views.home0, name="home0"),
+    path("home", views.home, name="home"),
+    path('home2/', views.home2, name='home2'),
+    path('home3/', views.home3, name='home3'),
+    path('cake/', views.cake, name='cake'),
+    path('home4/', views.home4, name='home4'),
 ]
--- website/views.py
+++ website/views.py
@@ -7,3 +7,13 @@ def background_music(request):
     return render(request, "backgroundmusic/music.html")
 def home(request):
     return render(request, "home.html")
+def home2(request):
+    return render(request, 'home2.html')
+def home3(request):
+    return render(request, 'home3.html')
+def cake(request):
+    return render(request, "cake.html")
+def home4(request):
+    return render(request, "home4.html")
+def home0(request):
+    return render(request, "home0.html")
